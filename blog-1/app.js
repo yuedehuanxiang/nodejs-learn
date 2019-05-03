78824b8173d9ed315c409fd10cd0b1acc47e0e9c
chenshuai <384619043@qq.com>
2019-05-04 07:42:49 +0800

    cookie登录验证

--- blog-1/app.js
+++ blog-1/app.js
@@ -48,6 +48,19 @@ const serverHandle = (req, res) => {
   // 解析 query
   req.query = querystring.parse(url.split("?")[1]);
 
+  // 解析 cookie
+  req.cookie = {};
+  const cookieStr = req.headers.cookie || "";
+  cookieStr.split("; ").forEach(item => {
+    if (!item) {
+      return;
+    }
+    const arr = item.split("=");
+    const key = arr[0];
+    const val = arr[1];
+    req.cookie[key] = val;
+  });
+
   // 解析 post data
   getPostData(req).then(postData => {
     req.body = postData;
--- blog-1/src/router/user.js
+++ blog-1/src/router/user.js
@@ -1,19 +1,44 @@
-const { loginCheck } = require("../controller/user");
+const { login } = require("../controller/user");
 const { SuccessModel, ErrorModel } = require("../model/resModel");
+
+// 获取 cookie 的过期时间
+const getCookieExpires = () => {
+  const d = new Date();
+  d.setTime(d.getTime() + 24 * 3600 * 1000);
+  console.log("d.toGMTString() is ", d.toGMTString());
+  return d.toGMTString();
+};
 
 const handleUserRouter = (req, res) => {
   const method = req.method; //GET POST
 
   // 登录
-  if (method === "POST" && req.path === "/api/user/login") {
-    const { username, password } = req.body;
-    const result = loginCheck(username, password);
+  if (method === "GET" && req.path === "/api/user/login") {
+    // const { username, password } = req.body;
+    const { username, password } = req.query;
+    const result = login(username, password);
     return result.then(data => {
       if (data.username) {
+        // 操作 cookie
+        res.setHeader(
+          "Set-Cookie",
+          `username=${
+            data.username
+          }; path=/; httpOnly; expires=${getCookieExpires()}`
+        );
         return new SuccessModel("登录成功");
       }
       return new ErrorModel("登录失败");
     });
+  }
+  // 登录验证的测试
+  if (method === "GET" && req.path === "/api/user/login-test") {
+    if (req.cookie.username) {
+      return Promise.resolve(
+        new SuccessModel({ username: req.cookie.username })
+      );
+    }
+    return Promise.resolve(new ErrorModel("尚未登录"));
   }
 };
 
